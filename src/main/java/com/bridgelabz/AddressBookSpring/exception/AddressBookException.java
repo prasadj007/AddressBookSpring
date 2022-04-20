e5cc379063ba19faf40acaa496a0015efb6f5255
Prasad <prasadj007official@gmail.com>
2022-04-20 16:01:17 +0530

    Provide Error message in case Validation fails

--- src/main/java/com/bridgelabz/AddressBookSpring/exception/AddressBookException.java
+++ src/main/java/com/bridgelabz/AddressBookSpring/exception/AddressBookException.java
@@ -1,5 +1,7 @@
 package com.bridgelabz.AddressBookSpring.exception;
 
+import org.springframework.http.HttpStatus;
+
 public class AddressBookException extends RuntimeException{
 
     public AddressBookException(String message){
